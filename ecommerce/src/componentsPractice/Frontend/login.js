2de1bf461a24a008458a1ca7d9d6f0a7b6e5614b
muna <munnu.karki1999@gmail.com>
2023-09-28 15:42:31 +0545

    register & login

--- ecommerce/src/componentsPractice/Frontend/login.js
+++ ecommerce/src/componentsPractice/Frontend/login.js
@@ -1,33 +1,84 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    // Prevent the default behaviour of form submit
+    e.preventDefault();
+
+    const data = {
+      email: email,
+      password: password,
+    };
+    axios
+      .post("http://localhost:5000/user/login", data)
+      .then((response) => {
+        console.log(response.data);
+
+        alert(`success: ${response.data.msg}`);
+        setTimeout(() => {
+          // Store the token in the localstorage
+          localStorage.setItem("token", response.data.token);
+          localStorage.setItem("userData", JSON.stringify(response.data)); // Store the user data localstorage
+
+          // Redirect to login after 1 seconds
+          console.log(response.data.token);
+          window.location.href = "/productform";
+        }, 1000);
+      })
+      .catch((err) => {
+        if (err.response) {
+          // The request was made and the server responded with a status code
+          // Extract the error message from the response data
+          const errorMessage = err.response.data[0];
+          alert(`Error: ${errorMessage}`);
+        } else {
+          // Error occurred before the request was made or no response was received
+          alert("Sorry, something went wrong");
+          console.log(err);
+        }
+      });
+  };
+
   return (
     <>
-    <form>
-  <div className="form-group">
-    <label htmlFor="email">Email:</label>
-    <input
-      type="email"
-      className="form-control"
-      id="email"
-      placeholder="Enter your email"
-    />
-  </div>
-  <div className="form-group">
-    <label htmlFor="password">Password:</label>
-    <input
-      type="password"
-      className="form-control"
-      id="password"
-      placeholder="Enter your password"
-    />
-  </div>
-  <button type="submit" className="btn btn-primary">
-    Login
-  </button>
-</form>
+      <div className="container">
+        <form>
+          <div className="form-group">
+            <label htmlFor="inputEmail">email</label>
+            <input
+              type="email"
+              className="form-control"
+              id="inputEmail"
+              placeholder="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="inputPassword">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="inputPassword"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={handleSubmit}
+          >
+            Sign in
+          </button>
+        </form>
+      </div>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- ecommerce/src/componentsPractice/Frontend/register.js
+++ ecommerce/src/componentsPractice/Frontend/register.js
@@ -1,60 +1,104 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 function Register() {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const data = {
+      firstname: firstname,
+      lastname: lastname,
+      email: email,
+      // username: username,
+      password: password,
+    };
+    axios
+      .post("http://localhost:5000/user/register", data)
+      .then((response) => {
+        console.log(response);
+        alert(`success: ${response.data.msg}`);
+
+        setTimeout(() => {
+          // Redirect to login after 1 seconds
+          window.location.href = "/login";
+        }, 1000);
+      })
+      .catch((err) => {
+        alert(`Error: ${err.response.data.msg}`);
+        console.log(err.response.data.msg);
+      });
+  };
+
   return (
     <>
-<form>
-  <div className="form-group">
-    <label htmlFor="firstName">First Name:</label>
-    <input
-      type="text"
-      className="form-control"
-      id="firstName"
-      placeholder="Enter your first name"
-    />
-  </div>
-  <div className="form-group">
-    <label htmlFor="lastName">Last Name:</label>
-    <input
-      type="text"
-      className="form-control"
-      id="lastName"
-      placeholder="Enter your last name"
-    />
-  </div>
-  <div className="form-group">
-    <label htmlFor="email">Email:</label>
-    <input
-      type="email"
-      className="form-control"
-      id="email"
-      placeholder="Enter your email"
-    />
-  </div>
-  <div className="form-group">
-    <label htmlFor="password">Password:</label>
-    <input
-      type="password"
-      className="form-control"
-      id="password"
-      placeholder="Enter your password"
-    />
-  </div>
-  <div className="form-group">
-    <label htmlFor="confirmPassword">Confirm Password:</label>
-    <input
-      type="password"
-      className="form-control"
-      id="confirmPassword"
-      placeholder="Confirm your password"
-    />
-  </div>
-  <button type="submit" className="btn btn-primary">
-    Register
-  </button>
-</form>
-</>
-  )
+      <div className="container">
+        <form>
+        <div className="form-group">
+            <label htmlFor="firstname">First Name</label>
+            <input
+              type="name"
+              className="form-control"
+              id="inputfirstname"
+              placeholder="Firstname"
+              onChange={(e) => setFirstname(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="lastname">Last Name</label>
+            <input
+              type="name"
+              className="form-control"
+              id="inputlastname"
+              placeholder="lastname"
+              onChange={(e) => setLastname(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="inputEmail">Email</label>
+            <input
+              type="email"
+              className="form-control"
+              id="inputEmail"
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          {/* <div className="form-group">
+            <label htmlFor="inputText">Username</label>
+            <input
+              type="text"
+              className="form-control"
+              id="inputText"
+              placeholder="Username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div> */}
+          <div className="form-group">
+            <label htmlFor="inputPassword">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="inputPassword"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={handleSubmit}
+          >
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default Register
+export default Register;
